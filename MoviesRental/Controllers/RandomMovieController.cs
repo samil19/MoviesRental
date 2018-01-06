a37e37abe0b5a21008a1675f81c30bf20a045694
Francisco Samyl Jimenez Dickson <samil.ibm@outlook.com>
2018-01-06 17:25:59 -0400

    First part of the Course

---
+++ MoviesRental/Controllers/RandomMovieController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MoviesRental.Models;
+
+namespace MoviesRental.Controllers
+{
+    public class RandomMovieController : Controller
+    {
+        // GET: RandomMovie
+        public ActionResult RandomMovie()
+        {
+            var movie = new List<Movies>
+            {
+                new Movies {Name = "Enemy of the State"},
+                new Movies {Name = "Hunger Games"}
+
+            };
+            var customer = new List<Customer>
+            {
+               new Customer {Name = "NSA", ID = 1},
+                new Customer {Name = "CIA", ID = 2},
+                new Customer {Name = "FBI", ID = 3}
+            } ;
+
+            var ViewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customer
+            };
+            return View(ViewModel);
+        }
+
+        public ActionResult Details(string name)
+        {
+            var hello = "Soy samiil";
+            var again = new Customer {Name = name};
+
+            return View(again);
+        }
+
+        public ActionResult ShowCustomers()
+        {
+            var customer = new List<Customer>
+            {
+                new Customer {Name = "NSA", ID = 1},
+                new Customer {Name = "CIA", ID = 2},
+                new Customer {Name = "FBI", ID = 3}
+            };
+
+            var viewModel1 = new RandomMovieViewModel
+            {
+                Customers = customer
+            };
+
+            return View(viewModel1);
+        }
+
+        public ActionResult ShowMovies()
+        {
+            var movie = new List<Movies>
+            {
+                new Movies {Name = "Enemy of the State"},
+                new Movies {Name = "Hunger Games"}
+            };
+        var viewModel = new RandomMovieViewModel
+        {
+            Movie = movie
+        };
+            return View(viewModel);
+        }
+    }
+}
---
+++ MoviesRental/Models/RandomMovieViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoviesRental.Models
+{
+    public class RandomMovieViewModel
+    {
+        public List<Movies> Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
